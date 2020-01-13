646c450664cdac9878f89130ece753825dc09059
Greg <greg@propaganda.gold>
2020-01-13 15:12:12 -0500

    should work now

--- install/node.sh
+++ install/node.sh
@@ -1,3 +1,2 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
-source ~/.bashrc
-sudo nvm install --lts
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install -y nodejs
