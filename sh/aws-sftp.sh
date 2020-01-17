41dc1df7f0b6ace788fed79c60458757bd146eb7
Greg <greg@propaganda.gold>
2020-01-17 09:37:57 -0500

    added sftp

---
+++ sh/aws-sftp.sh
@@ -0,0 +1,14 @@
+#
+# Run ssh with:
+#  1) standard key
+#  2) 'ubuntu' as the user
+
+REQUEST_HOSTNAME=$1
+
+if [ -z "$REQUEST_HOSTNAME" ]
+then
+	echo "Usage: aws-ssh.sh [hostname]"
+	exit 1
+fi
+
+sftp -i ~/Downloads/aws-key-jan2020-1.pem ubuntu@${REQUEST_HOSTNAME}
