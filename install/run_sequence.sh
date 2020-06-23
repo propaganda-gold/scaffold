302e686d80896f01c3e61b16baeaf04f4bd6be37
Greg Coppola <greg@propaganda.gold>
2020-06-23 01:20:42 +0000

    removing vim

--- install/run_sequence.sh
+++ install/run_sequence.sh
@@ -6,6 +6,9 @@ sudo snap install ripgrep --classic
 sudo snap install go --classic
 go get github.com/bazelbuild/buildtools/buildozer
 
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
+git clone https://github.com/lvht/fzf-mru.vim ~/.vim/pack/default/start/fzf-mru
+git clone https://github.com/junegunn/goyo.vim ~/.vim/pack/default/start/goyo
+
 ./install/bashrc
 ./install/vim
-
